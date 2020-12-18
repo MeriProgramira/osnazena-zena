1c2df072661462c14237b269160cfccea1bb7514
Indira147 <indiranuhanovic0@gmail.com>
2020-12-18 13:03:00 +0100

    girlthink.php(link inspiracija??)

---
+++ resources/views/partials/girlthink.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>One nas inspirisu</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+		
+	
+	<div class="container">
+			<div class="row">
+			
+			<img class="main-image" src="vjezba/gt_slika2.jpg">
+			</div>
+			
+			<h1>GirlTHing</h1>
+			<p><b><i>Udruženje mladih žena/djevojaka Girl THing je organizacija osnovana krajem 2018. godine sa misijom da osnaži djevojke da zamisle i ostvare svoje buduće snove u  oblasti informaciono-komunikacijskih tehnologija.
+		 Uzimajući u obzir da je svega 30% žena/djevojaka zaposleno u IT sektoru i da se većina djevojaka nakon završenog IT fakulteta opredijeli za profesiju asistentice ili profesorice u školi, samo od sebe nameće se pitanje, šta uraditi kako bi žene/djevojke ohrabrili da se upuste u veće izazove i krenu u ozbiljnije IT karijere. Vođena ovom mišlju, grupa mladih entuzijastica, dolazi na ideju o formiranju grupe djevojaka koja će zajedno raditi na projektima i ohrabrivati jedna drugu kroz IT svijet.
+		 Tako je nastao "Girl THing" mjesto za djevojke koje zajedno kreiraju i realizuju ideje, zajedno rade na novim inovacijama, pišu projekte, uče, prenose znanje i druže se. Nakon uspješno obilježenog Međunarodnog dana djevojaka u IKT-u 2019. godine, te  uspjesno realizovanih projekata E-Prisustvo,  Explore new borders i Ko kaže da žene ne znaju parkirati , ove mlade dame idu dalje. Predvođene sjajnim dr. sc. Almom Šećerbegović, Lejlom Bašić, Adnom Šaković, Nermom Jahić,Alminom Kovačević,Lejlom Šarić i ojacane sa 40-tak aktivnih clanica rade na 
+		 realizaciji novih projekata i pozivaju mlade, za IT sektor zainteresovane djevojke da im se pridruže. Vise informacija možete pronaći na<a href="http://www.girlthing.ba/"></a><i></b></p>
+		 <hr>
+	
+		<div class="row images-block">
+				<div class="col-lg-4 col-md-4 col-xs-12">
+				<img src="C:\Users\PC\Desktop\vjezba\IMG_8863">
+				</div>
+				<div class="col-lg-4 col-md-4 col-xs-12">
+				<img src="C:\Users\PC\Desktop\vjezba\IMG_8848">
+				</div>
+				<div class="col-lg-4 col-md-4 col-xs-12">
+				
+				<img src="C:\Users\PC\Desktop\vjezba\gt_slika1">
+				</div>
+			</div>
+			</div>
+		</body>
+	
+<style>
+		
+		.container{
+			margin-top:100px;
+			height:100%;
+			width:100%;
+		}
+		.images-block img{
+			height:20px;
+		}
+		
+		.main-image{
+		height:100%;
+		width:100%;
+		}
+		h1{
+		text-align:center;
+		}
+</style>	
+</html>
+
+//ostavila sam ovako da mozes pitati u Nahli, moze li ovaj tekst.
+//Cure su dale fotografije na koristenje, tekst je moj, a oslanja se na njihove informacije i tekstove.
+//Ostalim curama moze koristiti mogucnost da uce dalje sa njima. 
