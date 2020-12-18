c6821e3e5222ef25febaa870af420357768e6999
Indira147 <indiranuhanovic0@gmail.com>
2020-12-18 15:51:12 +0100

    malala.php

---
+++ resources/views/partials/malala.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	   
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+		<div class="row">
+				<div class="col-lg-3 col-12">
+					
+						<img src="https://www.seekpng.com/png/detail/359-3595903_pledges-worth-nearly-3-billion-dollars-to-help.png">
+								
+				</div>			
+					<div class="col-lg-9 col-12">
+						<div id="sadrzaj2" class="row information-block">
+							<h1>Malala Yousafzai</h1>
+							<p>Ova iznimna Pakistanka, dobitnica brojnih svjetskih nagrada i najmlađa osoba koja je ikada dobila Nobelovu nagradu za mir, u svom je kratkom životu već postigla mnogo.
+
+							A njena je priča zaista nevjerojatna. S 11 godina Malala je počela pisati dnevnik o životu školarke u pakistanskoj dolini Swat.
+
+							‘Možda više nikad neću ići u školu’, rečenica koju je objavila sredinom siječnja 2009. godine, nije rezultat njenog djetinjastog bunta ili odluke da prestane sa školovanjem, već odluke talibana da zatvore sve škole za djevojčice u okrugu Swat. Do tog trenutka već je 150 škola jednostavno bilo razneseno eksplozivom kako bi talibani bili sigurni da se djevojčice neće moći vratiti u školu.
+
+							I upravo ovdje počinje Malalina borba za pravo ne samo djevojčica, već sve djece svijeta, na obrazovanje i školovanje.
+
+							Unatoč prijetnjama kojima su ona i njena obitelj bile izložene, Malala se nastavila zalagati za obrazovanje djevojčica govoreći o tom gorućem pitanju u brojnim televizijskim intervjuima. Svoju je priču ispričala i u dva dokumentarca koji su ono što se događa u Pakistanu pokazali cijelom svjetu.
+
+							Njeno zalaganje u javnosti i otvoreno suprotstavljanje talibanskom režimu u kojem žene imaju podčinjenu ulogu, rezultiralo je pokušajem ubojstva dok se autobusom vraćala kući iz škole u listopadu 2012. godine. Ubojica kojeg su angažirali talibani ušao je u autobus i ispalio metak izravno u Malalino lice, pri tome ranivši još dvije djevojčice.
+
+							‘Često razmišljam o tome i zamišljam tu situaciju. Čak i ako me pokušaju ubiti, reći ću im da je to što čine krivo, da je obrazovanje naše temeljno pravo’, poručila je nakon atentata Malala.
+
+							No, da je pero jače od mača, odnosno metka u ovom slučaju, pokazala je i Malala koja je preživjela pokušaj ubojstva, napustila Pakistan, nastavila svoju borbu i 2014. dobila Nobelovu nagradu za mir.</p>
+						</div>				
+					</div>			
+		</div>
+</div>
+</body>
+<style>
+
+
+
+
+
+
+</style>
+</html>
