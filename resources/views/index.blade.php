59bdcf12c0b286cb61c157670eec532cfb9cc323
Indira147 <indiranuhanovic0@gmail.com>
2020-12-24 07:28:33 +0100

    footer translate

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,10 @@
 @include('partials/mission')
 
 <div class="empower justify-content-center d-flex flex-column align-items-center text-white px-3 text-center">
-    <h1 class="empower-title">empower women</h1>
+    <h1 class="empower-title">Osnažite žene</h1>
     <div class="container">
-        <p class="empower-text">make stronger and more confident, especially in <br> controlling  their life,
-            sharing a resource and claiming their rights</p>
+        <p class="empower-text">da postanu  snažnije i samopuzdanije, <br> da uzmu vlastiti život u svoje ruke,<br> 
+        da se potpomažu i bore za svoja prava.<br></p>
     </div>
 
 </div>
