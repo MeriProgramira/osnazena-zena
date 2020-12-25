53bd90e0bdd9768b98f366cd310d5bab260851a2
Marushy <mersiha.programira@gmail.com>
2020-12-25 19:59:30 +0100

    just some snow

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="site-blocks-cover overlay">
+<div class="site-blocks-cover overlay" id="particles-js">
 
 </div>
 
@@ -12,7 +12,7 @@
 <div class="empower justify-content-center d-flex flex-column align-items-center text-white px-3 text-center">
     <h1 class="empower-title">Osnažite žene</h1>
     <div class="container">
-        <p class="empower-text">da postanu  snažnije i samopuzdanije, <br> da uzmu vlastiti život u svoje ruke,<br> 
+        <p class="empower-text">da postanu  snažnije i samopuzdanije, <br> da uzmu vlastiti život u svoje ruke,<br>
         da se potpomažu i bore za svoja prava.<br></p>
     </div>
 
