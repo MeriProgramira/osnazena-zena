3a17ee7b76e33fc07869d0aa337e6157092da1c0
Marushy <mersiha.programira@gmail.com>
2020-12-25 15:53:38 +0100

    change quotes.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" type="image/jpg" href="{{ asset('images/favicon.ico') }}"/>
 
-    <title>{{ config('app.name', 'Osnazena zena') }}</title>
+    <title>Osnazena zena</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -94,9 +94,122 @@
           "insertdatetime media table contextmenu paste hr" ],
         toolbar: "insertfile undo redo | formatselect | bold italic underline | alignleft aligncenter alignright alignjustify | blockquote hr | bullist numlist outdent indent | link image",
         images_upload_url: 'postAcceptor.php',
-         automatic_uploads: false
+        automatic_uploads: false
 
         });
-</script>
+    </script>
+
+
+{{-- <script>
+
+    tinymce.init({
+
+        selector: "textarea",
+
+        file_picker_callback: function (cb, value, meta) {
+
+        var input = document.createElement('input');
+
+        input.setAttribute('type', 'file');
+
+        input.setAttribute('accept', 'image/*');
+
+
+
+        /*
+
+        Note: In modern browsers input[type="file"] is functional without
+
+        even adding it to the DOM, but that might not be the case in some older
+
+        or quirky browsers like IE, so you might want to add it to the DOM
+
+        just in case, and visually hide it. And do not forget do remove it
+
+        once you do not need it anymore.
+
+        */
+
+
+
+        input.onchange = function () {
+
+        var file = this.files[0];
+
+
+
+        var reader = new FileReader();
+
+        reader.onload = function () {
+
+        /*
+
+        Note: Now we need to register the blob in TinyMCEs image blob
+
+        registry. In the next release this part hopefully won't be
+
+        necessary, as we are looking to handle it internally.
+
+        */
+
+        var id = 'blobid' + (new Date()).getTime();
+
+        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+
+        var base64 = reader.result.split(',')[1];
+
+        var blobInfo = blobCache.create(id, file, base64);
+
+        blobCache.add(blobInfo);
+
+
+
+        /* call the callback and populate the Title field with the file name */
+
+        cb(blobInfo.blobUri(), { title: file.name });
+
+        };
+
+        reader.readAsDataURL(file);
+
+        };
+
+
+
+        input.click();
+
+        },
+
+       plugins: [
+
+  'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+
+  'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+
+  'table emoticons template paste help'
+
+],            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+
+        save_onsavecallback: function () {
+
+                var content = tinymce.activeEditor.getContent();
+
+            },
+
+            setup: function(ed) {
+
+                ed.on('keyup', function(e) {
+
+                    console.log('Editor contents was modified. Contents: ' + ed.getContent());
+
+                });
+
+            }
+
+        });
+
+</script> --}}
+
+
 </body>
 </html>
--- resources/views/quotes.blade.php
+++ resources/views/quotes.blade.php
@@ -25,8 +25,95 @@
           </div>
     </div>
 
+
     <div class="container text-center" data-aos="fade-up" >
-      <div class="card my-2">
+        <h5 class="my-5"><b> Dešava se svima da izgube smjer,energiju, prepuste se osjećaju sumnje u sebe i odustanu.
+            Ovdje su neki inspirativni citati za žene koji će vas osnažiti, a izrekle su ih neke od najjačih žena u povijesti.</b></h5>
+
+       <div class="card my-2">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Volim vidjeti mladu djevojku kako izlazi i svijet grabi za revere. Život je težak. Moraš izaći i pobijediti ga.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Maya Angelou</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="100">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0" >
+            <p>“Kada dajemo od sebe najbolje što možemo, nikad ne znamo kakvo čudo se može pojaviti u našem životu ili u životu drugoga.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Helen Keller</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="200">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Vjerujem da svatko od nas ima osobni poziv koji je jedinstven kao otisak prsta – i da je najbolji način na koji možete otkriti što volite,
+                a zatim pronaći način da to ponudite drugima – služenje, naporan  rad i također omogućavanje energiji svemira  da vas vodi.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Oprah Winfrey</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="300">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Jedna od najhrabrijih stvari koje možete učiniti je prepoznati sebe, znati tko ste, u što vjerujete i gdje želite ići.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Sheila Murray Bethel</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="400">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Što god žene radile, one to moraju odraditi duplo bolje od muškaraca kako bi se mislilo da su upola dobre kao oni.  Srećom, to nije teško.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Charlotte Whitton</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="500">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Moramo vjerovati da smo nadareni za nešto – i tu stvar, bez obzira na cijenu, moramo postići.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Marie Curie</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="600">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Žena je puni krug. Unutar nje je moć stvaranja, njege i transformacije.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Diane Mariechild</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="700">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Jaka žena shvaća da su darovi poput logike, odlučnosti i snage jednako ženstveno kao i intuicija i emocionalne veze.
+                Ona vrednuje i koristi sve svoje darove.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Nancy Rathburn</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="800">
+        <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>“Žena je puni krug. Unutar nje je moć stvaranja, njege i transformacije.”</p>
+            <footer class="blockquote-footer"> <cite title="Source Title">Diane Mariechild</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="900">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Jedna od lekcija s kojom sam odrasla, bila je da uvijek ostanem vjerna sebi i da nikad ne dopustim
@@ -36,7 +123,7 @@
         </div>
       </div>
 
-      <div class="card my-2" data-aos="fade-up" data-aos-delay="100">
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="1000">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Nadam se da će očevi i majke mladih djevojaka gledati ih u oči i reći im “Da, žene to mogu. “."</p>
@@ -45,7 +132,7 @@
         </div>
       </div>
 
-      <div class="card my-2" data-aos="fade-up" data-aos-delay="200">
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="1100">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Ženom se ne rađa, ženom se postaje.“</p>
@@ -54,7 +141,7 @@
         </div>
       </div>
 
-      <div class="card my-2" data-aos="fade-up" data-aos-delay="300">
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="1200">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Možda ćete doživjeti mnoge poraze, ali ne morate biti poraženi. U stvari, možda će biti potrebno da
@@ -64,7 +151,7 @@
         </div>
       </div>
 
-      <div class="card my-2" data-aos="fade-up" data-aos-delay="400">
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="1300">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Hrabrost je poput mišića. Korištenjem je jačamo. “</p>
@@ -73,7 +160,7 @@
         </div>
       </div>
 
-      <div class="card my-2" data-aos="fade-up" data-aos-delay="500">
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="1400">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Kada svoj život uzmete u svoje ruke – što se događa? Strašna stvar, ne možete nikoga kriviti za ništa. “</p>
@@ -82,7 +169,7 @@
         </div>
       </div>
 
-      <div class="card my-2" data-aos="fade-up" data-aos-delay="600">
+      <div class="card my-2" data-aos="fade-up" data-aos-delay="1500">
         <div class="card-body">
           <blockquote class="blockquote mb-0">
             <p>“Ako vam se ne sviđa biti otirač, onda se dignite s poda.”</p>
