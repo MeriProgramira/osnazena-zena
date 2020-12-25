18472f06c245837258cbbf558175dc6e1edddec5
Marushy <mersiha.programira@gmail.com>
2020-12-25 19:46:42 +0100

    add new year feeling

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,7 +17,11 @@
     <link rel="stylesheet" href="{{asset('css/style.css?v=').time()}}">
 </head>
 
-<body>
+<body >
+
+    <div id="particles-js">
+
+    </div>
 
     @include('./partials/main_nav')
 
@@ -32,6 +36,7 @@
     <script src="js/aos.js"></script>
     <script src="js/jquery.fancybox.min.js"></script>
     <script src="js/jquery.sticky.js"></script>
+    <script src="js/particles.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
     </script>
--- resources/views/partials/quotes.php
+++ resources/views/partials/quotes.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		
+
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	</head>
 	<body>
-		
+
 		<div class="container">
 		<h3>Dešava se svima da izgube smjer,energiju, prepuste se osjećaju sumnje u sebe i odustanu.
 			Ovdje su neki inspirativni citati za žene koji će vas osnažiti, a izrekle su ih neke od najjačih žena u povijesti.</h3>
 			<div class="row">
-				<img class="main-image" src="">		
+				<img class="main-image" src="">
 				</div>
 			<li>“Volim vidjeti mladu djevojku kako izlazi i svijet grabi za revere. Život je težak. Moraš izaći i pobijediti ga.” – Maya Angelou</li>
 			<li>“Kada dajemo od sebe najbolje što možemo, nikad ne znamo kakvo čudo se može pojaviti u našem životu ili u životu drugoga.” – Helen Keller</li>
-			<li>“Vjerujem da svatko od nas ima osobni poziv koji je jedinstven kao otisak prsta – i da je najbolji način na koji možete otkriti što volite, a zatim pronaći način da to ponudite drugima – služenje, naporan  rad i također omogućavanje energiji svemira  da vas vodi. “- Oprah Winfrey</li>
+			<li>“Vjerujem da svatko od nas ima osobni poziv koji je jedinstven kao otisak prsta – i da je najbolji način na koji možete otkriti što volite,
+                a zatim pronaći način da to ponudite drugima – služenje, naporan  rad i također omogućavanje energiji svemira  da vas vodi. “- Oprah Winfrey</li>
 			<li>Jedna od najhrabrijih stvari koje možete učiniti je prepoznati sebe, znati tko ste, u što vjerujete i gdje želite ići.” – Sheila Murray Bethel</li>
 			<li>“Što god žene radile, one to moraju odraditi duplo bolje od muškaraca kako bi se mislilo da su upola dobre kao oni.  Srećom, to nije teško. “- Charlotte Whitton</li>
 			<li>“Moramo vjerovati da smo nadareni za nešto – i tu stvar, bez obzira na cijenu, moramo postići.” – Marie Curie</li>
 			<li>“Žena je puni krug. Unutar nje je moć stvaranja, njege i transformacije. “- Diane Mariechild</li>
-			<li>“Jaka žena shvaća da su darovi poput logike, odlučnosti i snage jednako ženstveno kao i intuicija i emocionalne veze. Ona vrednuje i koristi sve svoje darove. “- Nancy Rathburn</li>
+			<li>“Jaka žena shvaća da su darovi poput logike, odlučnosti i snage jednako ženstveno kao i intuicija i emocionalne veze.
+                Ona vrednuje i koristi sve svoje darove. “- Nancy Rathburn</li>
 			<li>“Žena je puni krug. Unutar nje je moć stvaranja, njege i transformacije. “- Diane Mariechild</li>
-			
-			
-		
+
+
+
 		</div>
-		
+
 	</body>
 	<style>
-		
+
 		h1{
 		text-align:left;
 		}
@@ -38,8 +40,8 @@
 		.main-image{
 			width:100%;
 		}
-		
-	
+
+
 	</style>
 </html>
 
